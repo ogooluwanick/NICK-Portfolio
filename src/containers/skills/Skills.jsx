03a19864a9ddf729cda33c856b4d95f6d06d08c7
Ogooluwanick <ogooluwanick@gmail.com>
2022-03-01 21:29:22 +0200

    recents

--- src/containers/skills/Skills.jsx
+++ src/containers/skills/Skills.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import "./Skills.scss"
+import ReactTooltip from "react-tooltip"
+
+import AppWrap from "../../wrapper/AppWrap"
+import {motion} from "framer-motion"
+import { urlFor, client } from '../../client';
+
+
 
 
 const Skills = () => {
+    const [experiences, setExperiences] = useState([]);
+    const [skills, setSkills] = useState([]);
+
+    useEffect(() => {
+      const ep_query = '*[_type == "experiences"]';
+      const sk_query = '*[_type == "skills"]';
+
+      client.fetch(ep_query).then((data) => {
+        setExperiences(data);
+      })
+
+      client.fetch(sk_query).then((data) => {
+        setSkills(data);
+      })
+    }, [])
   return (
-    <div>Skills</div>
+    <div>
+      <h2 className="head-text">Skills & Experience  </h2>
+      <div className="app__skills-container">
+        <motion.div className='app__skills-list'>
+            {
+              skills.map((skill)=>(
+                <motion.div whileInView={{opacity:[0,1 ]}} transition={{duration:0.5}} className="app__skills-item app__flex" key={skill.name}>
+                    <div className="app__flex" style={{backgroundColor:skill.bgColor}}>
+                      <img src={urlFor(skill.icon)} alt={skill.name} />
+                    </div>
+                </motion.div>
+              ))
+            }
+        </motion.div>
+      </div>
+
+    </div>
   )
 }
 
--- src/containers/work/Work.jsx
+++ src/containers/work/Work.jsx
@@ -1,6 +1,7 @@
 import React,{useEffect,useState} from 'react'
 import AppWrap from "../../wrapper/AppWrap"
 import {motion} from "framer-motion"
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { urlFor, client } from '../../client';
 
 
@@ -9,12 +10,23 @@ import "./Work.scss"
 
 const Work = () => {
   const [activeFilter, setActiveFilter] = useState("All")
-  const [animateCard, setanimateCard] = useState({y:0,opacity: 1 })
+  const [animateCard, setAnimateCard] = useState({y:0,opacity: 1 })
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([]);
 
   const handleWorkFilter=(item)=>{
+    setActiveFilter(item)
+    setAnimateCard([{y:100,opacity:0}])
 
+    setTimeout(()=>{
+      setAnimateCard([{y:0,opacity:1}]);
+        if (item==="All"){
+          setFilterWork(works)
+        }
+        else {
+          setFilterWork(works.filter((works)=>works.tags.includes(item)))
+        }
+    },500)
   }
 
   useEffect(() => {
@@ -35,28 +47,68 @@ const Work = () => {
 
       <div className="app__work-filter">
         {
-          ["Inventory Management System","Pyhon Project","Mobile App","Animation Project","Game","All"].map((item,index)=>(
-            <div key={index} onClick={()=>handleWorkFilter(item)} className={`app__work-filter-item app__flex p-text ${activeFilter=== item?"item-active":""}}`}>
+          ["All","React","Python","Mobile App","Animations","Graphic Design","UI/UX"].map((item,index)=>(
+            <div key={index} onClick={()=>handleWorkFilter(item)} className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}>
               {item}
             </div>
           ))
         }
       </div>
 
-      <motion.div animate={animateCard} transition={{duration:0.5, delayChildren:0.5}} className="app__work-portfolio">
-        {
-          filterWork.map((work,index)=>(
-            <div className="app__work-item app__flex" key={index}>
-              <div className='app__work-img app__flex'>
-                <img src={urlFor(work.imgUrl)} alt={work.work} />
-              </div>
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-portfolio"
+      >
+        {filterWork.map((work, index) => (
+          <div className="app__work-item app__flex" key={index}>
+            <div
+              className="app__work-img app__flex"
+            >
+              <img src={urlFor(work.imgUrl)} alt={work.name} />
 
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                className="app__work-hover app__flex"
+              >
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
+
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+                <a href={work.codeLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </motion.div>
             </div>
-          ))
-        }
+
+            <div className="app__work-content app__flex">
+              <h4 className="bold-text">{work.title}</h4>
+              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
+
+              <div className="app__work-tag app__flex">
+                <p className="p-text">{work.tags[0]}</p>
+              </div>
+            </div>
+          </div>
+        ))}
       </motion.div>
     </>
   )
 }
 
-export default Work
+export default AppWrap(Work,"work")
