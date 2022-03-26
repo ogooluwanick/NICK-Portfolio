b92dc2d3ca05558d774f72978a20b039177407d5
Ogooluwanick <ogooluwanick@gmail.com>
2022-03-27 00:15:18 +0200

    google search console

--- src/componets/Navbar/Navbar.jsx
+++ src/componets/Navbar/Navbar.jsx
@@ -9,7 +9,10 @@ const Navbar = () => {
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
-        <img src={images.logopng}  alt="logo" />
+        <a href="#home">
+          <img src={images.logopng}  alt="logo" />
+        </a>
+        
       </div>
       <ul className='app__navbar-links'>
         {
